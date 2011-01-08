72aadfe56f6af935797fcf42870e37a1a5af7f40
Renaud Bruyeron <bruyeron@fullsix.com>
2011-01-08 16:17:04 +0100

    remove remote services

--- src/com/ekino/web/rpnCalculatorApplication/client/RpnCalculatorApplicationServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package com.ekino.web.rpnCalculatorApplication.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface RpnCalculatorApplicationServiceAsync {
-    void getMessage(String msg, AsyncCallback<String> async);
-}
